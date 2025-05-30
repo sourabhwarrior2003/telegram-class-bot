eabd4b4371d35cff41c21bcfca95d12499aec332
sourabhwarrior2003 <sourabhgorkhe44@gmail.com>
2025-05-30 14:00:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+# Replace with your actual bot token and chat ID
+TOKEN = "7925848334:AAGCdfQqSKj2KDgQ-zlCo2yJ9xSf1JNMCLA"
+CHAT_ID = "1694959583"
+MESSAGE = "📢 Hello Sourabh! Your class has been moved to 4 PM."
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+payload = {
+    "chat_id": CHAT_ID,
+    "text": MESSAGE
+}
+
+res = requests.post(url, data=payload)
+print("Message status:", res.status_code)
