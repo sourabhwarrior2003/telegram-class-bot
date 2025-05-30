b2f768e6d27b3e45fa25e524dd06962e75963f0b
sourabhwarrior2003 <sourabhgorkhe44@gmail.com>
2025-05-30 22:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 # Replace with your actual bot token and chat ID
 TOKEN = "7925848334:AAGCdfQqSKj2KDgQ-zlCo2yJ9xSf1JNMCLA"
 CHAT_ID = "1694959583"
-MESSAGE = "📢 Hello Sourabh! Your class has been moved to 4 PM."
+MESSAGE = "📢 Hello classs Your class has been moved to 4 PM."
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {
